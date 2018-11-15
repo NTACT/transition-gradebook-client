12638915426bc593151c99703a2421c0962deb31
James Meyers <james.meyers@emberex.com>
2018-11-15 11:12:32 -0800

    Don't show student filter form by default

--- src/screens/Students.js
+++ src/screens/Students.js
@@ -29,7 +29,7 @@ import * as breakpoints from '../breakpoints';
 @observer
 class Students extends Component {
   @observable search = '';
-  @observable filtersOpen = true;
+  @observable filtersOpen = false;
   @observable filter = null;
   @observable loadTask = null;
 
