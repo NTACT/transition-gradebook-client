30b7324c33fe5b80fe3116996207a9ef2d490cc4
Ryan Peterson <ryan.peterson@emberex.com>
2019-02-26 14:37:43 -0800

    Add a type to the Blob constructor so that IOS properly detects the pdf link

--- src/models/Store.js
+++ src/models/Store.js
@@ -324,7 +324,7 @@ export default class Store {
       throw error;
     });
 
-    const url = window.URL.createObjectURL(new Blob([response.data]));
+    const url = window.URL.createObjectURL(new Blob([response.data], { type: 'application/pdf' }));
     const link = document.createElement('a');
     link.href = url;
     link.setAttribute('download', `${filename}.pdf`);
